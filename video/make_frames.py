38ccdf81ff6172ad4530084716cb337a74e7840c
Juan Ignacio Rodriguez de Leon euribates@gmail.com <euribates@gmail.com>
2017-03-15 10:32:57 +0000

    Añadido frame 16/9

--- video/make_frames.py
+++ video/make_frames.py
@@ -5,18 +5,10 @@ import subprocess
 import os
 from filters import slugify
 
-with open('charlas.txt', 'r') as f:
-    titulos = [x.strip() for x in f.readlines()]
-
-with open('frame-video-4-3.svg', 'r') as f:
-    template = f.read()
-
-for titulo in titulos:
-    print('generando frame charla {}'.format(titulo), end=' ')
-    salida = template.replace('{{titulo}}', titulo)
+def make_png(template, titulo, tag):
     with open('salida.svg', 'w') as f:
-        f.write(salida)
-    filename = '{}.png'.format(slugify(titulo))
+        f.write(template.replace('{{titulo}}', titulo))
+    filename = '{}-{}.png'.format(slugify(titulo), tag)
     subprocess.call([
         'inkscape',
         'salida.svg',
@@ -24,6 +16,21 @@ for titulo in titulos:
         '--export-dpi=90',
         ])
     os.unlink('salida.svg')
+
+
+with open('charlas.txt', 'r') as f:
+    titulos = [x.strip() for x in f.readlines() if x.strip()]
+
+with open('frame-video-4-3.svg', 'r') as f:
+    template_4_3 = f.read()
+
+with open('frame-video-16-9.svg', 'r') as f:
+    template_16_9 = f.read()
+
+for titulo in titulos:
+    print('generando frames charla {}'.format(titulo), end=' ')
+    make_png(template_4_3, titulo, '4-3')
+    make_png(template_16_9, titulo, '16-9')
     print('[OK]')
 
 
