3d0315474fee4f1d3f4c0d01ce68628acb2f2c26
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2018-11-05 22:36:36 +0000

    Camisetas para staff y asistentes

---
+++ camisetas/pydaytf18/make_samples.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+convert camiseta-staff-frontal.png -thumbnail 650x camiseta-staff-frontal-sample.png
+convert camiseta-staff-posterior.png -thumbnail 650x camiseta-staff-posterior-sample.png
+convert camiseta-asistente-frontal.png -thumbnail 650x camiseta-asistente-frontal-sample.png
+convert camiseta-asistente-frontal-variante.png -thumbnail 650x camiseta-asistente-frontal-variante-sample.png
+
